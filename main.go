ba54978892e9e8c0dc915d34837adafe140fb0d3
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2026-01-17 01:57:46 +0530

    Simple ToDo Application

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Task struct {
+	id uint
+	name string
+	description string 
+
+}
+
+var tasks []Task
+
+func main(){
+	fmt.Println("Welcome to Go TaskPlanner!")
+	fmt.Println("What should i call you? (Enter Username):")
+	var userName string
+	fmt.Scan(&userName)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Println("Enter Task details: ")
+		fmt.Println("Enter Task id: ")
+		var task_id int
+		fmt.Scan(&task_id)
+		fmt.Println("Enter Task name: ")
+		scanner.Scan()
+		task_name := scanner.Text()
+		fmt.Println("Enter Task description: ")
+		scanner.Scan()
+		task_desc := scanner.Text()
+		fmt.Println("---------------Task details:---------------")
+		fmt.Printf("Id: %v\n", task_id)
+		fmt.Printf("Name: %v\n", task_name)
+		fmt.Printf("Description: %v\n", task_desc)
+		fmt.Println("-------------------------------------------")
+		fmt.Println("Enter 0 to exit...")
+		var exit uint
+		fmt.Scan(&exit)
+		if exit == 0 {
+			break
+		}
+	}
+}
