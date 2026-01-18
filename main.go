3cb8628a2b0adf7490fb23d9f260f007e8599bc2
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2026-01-18 22:52:11 +0530

    Simple Http routes

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+var tasks = []string{"WakeUp", "Gym", "Breakfast"}
+
+func main() {
+	http.HandleFunc("/", helloUser)
+	http.HandleFunc("/show-tasks", showTasks)
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloUser(writer http.ResponseWriter, request *http.Request) {
+	var greeting = "Welcome to Go TaskPlanner!"
+	fmt.Fprintln(writer, greeting)
+}
+
+func showTasks(writer http.ResponseWriter, request *http.Request) {
+	for _, task := range tasks {
+		fmt.Fprintln(writer, task)
+	}
 }
